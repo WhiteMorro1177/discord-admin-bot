81af5933a7ea8436ba4c585eba37c3e64b1e474e
WhiteMorro1177 <jackforest228@gmail.com>
2023-10-05 20:12:09 +0300

    working on (/) commands

--- bot.js
+++ bot.js
@@ -1,25 +1,51 @@
 'use strict';
 
 // imports
-const { Client, Events, IntentsBitField, ChannelType } = require("discord.js");
+const { Client, Events, IntentsBitField, ChannelType, ButtonBuilder, SlashCommandBuilder } = require("discord.js");
 const _config = require("./config.json");
-const { Command } = require("./bot_entities")
+const request = require("request");
 
 // bot configuration
 const intents = [
 	IntentsBitField.Flags.Guilds, 
   	IntentsBitField.Flags.GuildMembers,
   	IntentsBitField.Flags.GuildMessages,
-  	IntentsBitField.Flags.MessageContent,
+  	IntentsBitField.Flags.MessageContent
 ]
 
+const slash_hello = new SlashCommandBuilder().setName("hello");
+const url = "https://discord.com/api/v10/applications/895734878249828363/commands";
+
+
+// // command creation
+// module.exports = {
+// 	data: new SlashCommandBuilder()
+// 	.setName("init").setDescription("Reinitialize bot"), 
+// 	async execute(interaction) {
+// 		await interaction.channel.send("init");
+// 	},
+	
+// 	data: new SlashCommandBuilder().setName("hello"),
+// 	async execute(interaction) {
+// 		await interaction.reply("Hi!");
+// 	}
+// }
+
 // bot init
+
+module.exports = {
+	data: slash_hello,
+	async execute(interaction) {
+		console.log("Command called");
+	}
+}
+
 const client = new Client({ intents: intents});
 
 // bot initialization
 client.once(Events.ClientReady, client => {
-	console.log(`Start configurate ${client.name}`); 
-	
+	console.log(`Start configurate ${client.user.tag}`); 
+
 	_config.server_info["users"] = client.users;
 	_config.server_info["roles"] = client.roles;
 	_config.server_info["text-channels"] = client.channels.cache.filter(channel => { if (channel.type == ChannelType.GuildText) return true; });
@@ -27,9 +53,8 @@ client.once(Events.ClientReady, client => {
 	
 	console.log(_config.server_info);
 	
-	
-
-  	_config.server_info["main-text-channel"].send(`Hi! Still Just_A_Bot, but now on JS! Say  '${_config.command_prefix}hello'  to me`);
+	console.log(`Logged as ${client.user.tag}`);
+  	_config.server_info["main-text-channel"].send(`${client.user.tag}, locked and loaded! Type '${_config.command_prefix}init' to start initialization`);
 });
 
 // message handling
@@ -41,12 +66,21 @@ client.on(Events.MessageCreate, message => {
 
 	// command handling
 	if (message.content.startsWith(_config.command_prefix)) { // if "message" is a command...
-		const cmd_text = message.content.replace(_config.command_prefix, "");
-
-		const cmd = new Command(message.content, message.author);
-
+		
 	}
 	// logging messages
+	// ...
+
+});
+
+// commands handling
+client.on(Events.InteractionCreate, interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	console.log("Interaction created");
+
+	if (interaction.commandName == "hello") {
+		interaction.reply("Hi");
+	}
 });
 
 // users handling => new user registration
@@ -54,5 +88,9 @@ client.on(Events.GuildMemberAdd, member => {
 
 });
 
+// roles handling
+client.on(Events.GuildRoleCreate, role => {
+
+});
 
 client.login(_config.token);
--- bot_entities.js
+++
@@ -1,9 +0,0 @@
-const _config = require("./config.json");
-
-export class Command {
-
-    constructor(raw_message, user) {
-        this.text = raw_message.replace(_config.command_prefix, "");
-        this.user = user
-    }
-}
