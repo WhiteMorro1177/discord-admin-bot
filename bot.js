bfa12423afc9f512ce7c4f8cb730d390e7b270ec
WhiteMorro1177 <jackforest228@gmail.com>
2023-11-02 15:33:48 +0300

    optimize channel creation

--- bot.js
+++ bot.js
@@ -5,8 +5,6 @@ const { Client, Events, IntentsBitField, ChannelType } = require("discord.js");
 const _config = require("./config.json");
 const _server_info = require("./server_info.json");
 
-const { joinVoiceChannel } = require('@discordjs/voice');
-
 // bot configuration
 const intents = [
 	IntentsBitField.Flags.Guilds, 
@@ -23,23 +21,20 @@ const client = new Client({ intents: intents});
 
 // bot initialization
 client.once(Events.ClientReady, client => {
-	console.log(`${client.user.tag}, locked and loaded! Type '${_config.command_prefix}init' to start initialization`);
+    console.log(client);
+
+    // TODO ( do refactor )
+    // _server_info.users_ids = client.guilds.cache.find(guild => guild.id == _server_info.guild_id).members.cache.map(member => member.id);
+    _server_info.roles_ids = client.guilds.cache.find(guild => guild.id == _server_info.guild_id).roles.cache.map(role => role.id);
 
     _server_info.main_voice_channel_id = client.channels.cache.find(channel => channel.name === "#Create_Channel_[+]").id;
-
-    // console.log(client.guilds.cache.at(0));
-
-	// _config.server_info["users-ids"] = client.users.cache.map(user => user.id);
-	// _config.server_info["roles-ids"] = client.guilds.cache.at(0).roles.cache.map(role => role.id);
-	// _config.server_info["text-channels-ids"] = client.channels.cache.filter(channel => channel.type === ChannelType.GuildText).map(sortedItem => sortedItem.id);
-	// _config.server_info["main-text-channel-id"] = client.channels.cache.find((channel) => channel.name === "bot-tests").id;
     
-	// console.log(_config.server_info);
-	
-	// console.log(`Logged as ${client.user.tag}`);
-    // client.channels.cache
-    //     .find(channel => channel.id == _config.server_info["main-text-channel-id"])
-    //     .send(`${client.user.tag}, locked and loaded! Type '${_config.command_prefix}init' to start initialization`);
+    // channel.type = 0 => GuildText
+    _server_info.text_channels_ids = client.channels.cache.filter(channel => channel.type === 0).map(channel => channel.id);
+    _server_info.voice_channels_ids = client.channels.cache.filter(channel => channel.type === 2).map(channel => channel.id);
+    
+    console.log(_server_info);
+	console.log(`${client.user.tag}, locked and loaded! Type '${_config.command_prefix}init' to start initialization`);
 });
 
 // message handling
@@ -75,9 +70,9 @@ client.on(Events.VoiceStateUpdate, (oldState, newState) => {
     const newStateChannel = newState.channel;
     const user = newStateChannel?.members.at(0).user;
 
-    if (newStateChannel?.id === creationChannel.id) {
+    if (newStateChannel?.id === creationChannel?.id) {
         const newChannelName = `${user.tag}'s Voice Channel`;
-        const creationChannelParent = newState.guild.channels.cache.find(channel => channel.id === "629317885935878156");
+        const creationChannelCategory = newState.guild.channels.cache.find(channel => channel.id === "629317885935878156");
 
         newState.channel.setName(newChannelName)
         .then(voice => {
@@ -85,62 +80,55 @@ client.on(Events.VoiceStateUpdate, (oldState, newState) => {
             console.log(_server_info.temp_voices_ids);
             newState.guild.channels.create({
                 name: "#Create_Channel_[+]",
-                type: ChannelType.GuildVoice,
-                parent: creationChannelParent,
+                type: 2, // GuildVoice
+                parent: creationChannelCategory,
                 position: 0
             })
             .then(voice => {
                 voice.setPosition(voice.position - 1);
-                _server_info.main_voice_channel_id = voice.id;
+                _server_info.voice_channels_ids.push(voice.id);
             });
         });
-
-        // newState.guild.channels.create({
-        //     name: newChannelName,
-        //     type: ChannelType.GuildVoice,
-        //     parent: _config.guild_id
-        // })
-        // .then(voice => {
-        //     const connection = joinVoiceChannel({
-        //         channelId: voice.id,
-        //         guildId: _config.guild_id,
-        //         adapterCreator: newState.guild.voiceAdapterCreator
-        //     });
-
-        //     connection.
-        //     _server_info.temp_voices_ids.push(voice.id);
-        // });
     }
         
     const leavedChannel = oldState.channel;
     const channelToDeleteId = _server_info.temp_voices_ids.find(id => id === leavedChannel?.id);
+    
     if (channelToDeleteId !== undefined) {
         if (leavedChannel?.members.size === 0) {
             oldState.guild.channels.delete(leavedChannel)
             _server_info.temp_voices_ids = _server_info.temp_voices_ids.filter(id => id !== channelToDeleteId);
+            // TODO( rewrite with .splice() )
         }
     }
-    
-    // const tempVoices = _config.server_info["temp-voices"];
-    
-    // const channelToDelete = tempVoices.find(channel => channel.id === leavedChannel.id)
-    // if (channelToDelete !== undefined) {
-    //     leavedChannel.guild.channels.delete(channelToDelete).then(() => {
-    //         console.log("Channel {0} deleted", channelToDelete.id);
-    //     });
-    // }
 
-    console.log(_server_info.temp_voices_ids);
+    console.log("temp voices: " + _server_info.temp_voices_ids);
 });
 
 client.on(Events.ChannelCreate, channel => {
     console.log("New channel created");
-    console.log(channel);
+    console.log(channel.name);
+
+    if (channel.type === 2) { // GuildVoice
+        const findedChannelId = _server_info.voice_channels_ids.find(id => id === channel.id);
+
+        if (findedChannelId === undefined) {
+            _server_info.voice_channels_ids.push(findedChannelId);
+        } else {
+            console.log(findedChannelId + ": this channel already exist");
+            channel.delete();
+        }
+    }
 });
 
 client.on(Events.ChannelDelete, channel => {
     console.log("Channel deleted");
-    console.log(channel);
+    console.log(channel.name);
+
+    if (channel.type === 2) { // GuildVoice
+        _server_info.voice_channels_ids = _server_info.voice_channels_ids.filter(id => id !== channel.id);
+        // TODO( rewrite with .splice() )
+    }
 });
 
 // users handling => new user registration
